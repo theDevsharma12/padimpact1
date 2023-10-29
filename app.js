8a148d209ada5c4f9243774d525c1586499fe7a6
devsharma12 <107252406+theDevsharma12@users.noreply.github.com>
2023-10-29 11:26:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,15 +45,6 @@ function chooseSlide(n) {
 	clearInterval(timer);
 }
 
-function showMenu() {
-	var topNav = document.getElementById('topnav');
-	if (topNav.className === "navbar") {
-		topNav.className += " show";
-	} else {
-		topNav.className = "navbar";
-	}
-}
-
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -82,25 +73,3 @@ window.onclick = function(event) {
 
 // -----------------------------
 
-const nav = document.querySelector(".nav"),
-  searchIcon = document.querySelector("#searchIcon"),
-  navOpenBtn = document.querySelector(".navOpenBtn"),
-  navCloseBtn = document.querySelector(".navCloseBtn");
-
-searchIcon.addEventListener("click", () => {
-  nav.classList.toggle("openSearch");
-  nav.classList.remove("openNav");
-  if (nav.classList.contains("openSearch")) {
-    return searchIcon.classList.replace("uil-search", "uil-times");
-  }
-  searchIcon.classList.replace("uil-times", "uil-search");
-});
-
-navOpenBtn.addEventListener("click", () => {
-  nav.classList.add("openNav");
-  nav.classList.remove("openSearch");
-  searchIcon.classList.replace("uil-times", "uil-search");
-});
-navCloseBtn.addEventListener("click", () => {
-  nav.classList.remove("openNav");
-});
